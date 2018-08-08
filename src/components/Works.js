e682ce65b8fb8811b8c4b96f60b1de7e73a4f326
Ewan Bauld <ewan.bauld@gmail.com>
2018-08-08 15:58:28 +0100

    Add more styling and images

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,32 +1,35 @@
 import React from 'react'
+import bitcoinImage from '../styles/img/bitcoin.jpg';
+import appleStore from '../styles/img/download-app.svg';
+import googlePlay from '../styles/img/download-app-android.png';
 
 function Works() {
     return (
         <div>
-            <section class="section-steps" id="works">
-                <div class="row">
+            <section className="section-steps" id="works">
+                <div className="row">
                     <h2>How it works &mdash; Simple as 1, 2, 3</h2>
                 </div>
-                <div class="row">
-                    <div class="col span-1-of-2 steps-box">
-                        <img src="resources/img/app-iPhone.png" alt="Bitcoin Merchant app on iPhone" class="app-screen js--wp-2"/>
+                <div className="row">
+                    <div className="col span-1-of-2 steps-box">
+                        <img src={bitcoinImage} alt="Bitcoin Merchant app on iPhone" className="app-screen js--wp-2"/>
                     </div>
-                    <div class="col span-1-of-2 steps-box">
-                        <div class="works-step">
+                    <div className="col span-1-of-2 steps-box">
+                        <div className="works-step">
                             <div>1</div>
                             <p>Register your details with ths site.</p>   
-                        </div> */}
-                        <div class="works-step">
+                        </div> 
+                        <div className="works-step">
                             <div>2</div>
                             <p>Enter your bank account details so we can verify who you are.</p>   
                         </div>
-                        <div class="works-step">
+                        <div className="works-step">
                             <div>3</div>
-                            <p>Begin right away!</p>   
+                            <p>Begin trading right away!</p>   
                         </div>
                         
-                        {/* <a href="#" class="btn-app"><img src="resources/img/download-app.svg" alt="App Store Button"></a>
-                        <a href="#" class="btn-app"><img src="resources/img/download-app-android.png" alt="Play Store Button"></a> */}
+                        <a href="#" className="btn-app"><img src={appleStore} alt="App Store Button"/></a>
+                        <a href="#" className="btn-app"><img src={googlePlay} alt="Play Store Button"/></a>
                     </div>
                 </div>
             </section>            
