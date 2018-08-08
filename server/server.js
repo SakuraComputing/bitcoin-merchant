63cb6e17dc7758ecad174e56a061f684d8076fa8
Ewan Bauld <ewan.bauld@gmail.com>
2018-08-08 17:46:10 +0100

    Install Express and connect to Mongo

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// DB Config
+
+const db = require('../config/keys').mongoURI;
+
+// Connect to Mongo DB
+mongoose
+    .connect(db)
+    .then(() => console.log('Mongo DB connected'))
+    .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello World'));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
