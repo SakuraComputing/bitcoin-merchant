0f2c76a982639e9a0af8936d87c52f6867a93bd3
Ewan Bauld <ewan.bauld@gmail.com>
2018-08-08 14:17:13 +0100

    Finsih styling on safety page

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Ionicon from 'react-ionicons';
 
 const Features = () => {
     return (
@@ -13,28 +14,28 @@ const Features = () => {
             
             <div className="row js--wp-1">
                 <div className="col span-1-of-4 box">
-                    <i className="ion-ios-infinite-outline icon-big"></i>
+                    <Ionicon fontSize="50%" icon="ios-infinite-outline"></Ionicon>
                     <h3>Up to 365 days/year</h3>
                     <p>
                         Never work again! We really mean that. Once you get into Bitcoin trading you will find our ease of use service so incredible you can get on with your life.
                     </p>
                 </div>
                 <div className="col span-1-of-4 box">
-                    <i className="ion-ios-stopwatch-outline icon-big"></i>
+                    <Ionicon fontSize="50%" icon="ios-stopwatch-outline" color="#B15B60"></Ionicon>
                     <h3>Make your first trade in 20 minutes</h3>
                     <p>
                         You're only twenty minutes away from your first adventure with the worlds premium cryptocurrency. We work with the best traders in the world to ensure that you're 100% happy.
                     </p>
                 </div>
                 <div className="col span-1-of-4 box">
-                    <i className="ion-ios-nutrition-outline icon-big"></i>
+                    <Ionicon fontSize="50%" icon="ios-cloud-upload" color="blue"></Ionicon>
                     <h3>100% Safe</h3>
                     <p>
                         All your transactions are completely secure! All our traders folow tight rules in order to guarantee a high level of satisfaction.
                     </p>
                 </div>
                 <div className="col span-1-of-4 box">
-                    <i className="ion-ios-cart-outline icon-big"></i>
+                    <Ionicon fontSize="50%" icon="ios-cart-outline" color="orange"></Ionicon>
                     <h3>Order Any Amount</h3>
                     <p>
                         We don't limit your trading, you can go up to the maximum that your traders allow. It's up to you!
