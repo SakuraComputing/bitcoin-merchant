b00a554d3af2bc519427e2175b9907d2aecd9075
Ewan Bauld <ewan.bauld@gmail.com>
2018-08-13 19:52:54 +0100

    Validate User API

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,7 @@ const passport = require('passport');
 
 // Load input validation
 const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 // Load user model
 const User = require('../models/user');
@@ -32,7 +33,8 @@ router.post('/register', (req, res) => {
     User.findOne({ email: req.body.email })
     .then(user => {
         if(user) {
-            return res.status(400).json({ email: 'Email alreqady exists'});
+            errors.email = 'Email already exists';
+            return res.status(400).json(errors);
         } else {
             const avatar = gravatar.url(req.body.email, {
                 s: '200',
@@ -63,6 +65,12 @@ router.post('/register', (req, res) => {
 // @desc Login User / Return JWT token
 // @access Public
 router.post('/login', (req, res) => {
+
+    const { errors, isValid } = validateLoginInput(req.body);
+    if(!isValid) {
+        return res.status(400).json(errors);
+    }
+
     const email = req.body.email;
     const password = req.body.password
 
@@ -94,7 +102,8 @@ router.post('/login', (req, res) => {
                     }
                 );
             } else {
-                return res.status(400).json({ password: 'Password incorrect'})
+                errors.password = 'Password Incorrect';
+                return res.status(400).json(errors);
             }
         })
     })
