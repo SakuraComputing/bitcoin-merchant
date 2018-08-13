47e4cb24493388ea466f3dd34008b9307f7d5886
Ewan Bauld <ewan.bauld@gmail.com>
2018-08-13 15:41:24 +0100

    Add User API for registration and login

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const users = require('../routes/api/users');
 const profile = require('../routes/api/profiles');
 
 const app = express();
 
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // DB Config
 
