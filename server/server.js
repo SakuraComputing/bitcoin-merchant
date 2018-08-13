1e014984b5797ccf3c97ca2e27cc3cc22e72cf3c
Ewan Bauld <ewan.bauld@gmail.com>
2018-08-13 17:51:09 +0100

    Add Json Web Tokens

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('../routes/api/users');
 const profile = require('../routes/api/profiles');
@@ -21,7 +22,10 @@ mongoose
     .then(() => console.log('Mongo DB connected'))
     .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.use(passport.initialize());
+
+// Passport Config
+require('../config/passport')(passport);
 
 // Use Routes 
 app.use('/api/users', users);
