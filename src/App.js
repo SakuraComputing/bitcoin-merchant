65d1175e8592da0aef609b245149786f75628645
Ewan Bauld <ewan.bauld@gmail.com>
2018-08-14 19:34:37 +0100

    Add REDUX and devtools

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import '../src/styles/styles.scss';
 import LandingPage from './components/Layout/LandingPage';
 import Register from './components/auth/Register';
 import Login from './components/auth/login';
 
+
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <Route exact path="/" component={LandingPage} />
+      <Provider store={store}>
+        <Router>
           <div>
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
+            <Route exact path="/" component={LandingPage} />
+            <div>
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
