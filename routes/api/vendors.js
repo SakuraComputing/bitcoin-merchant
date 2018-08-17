e2c54985655258c05014c47c77add98ccc8dc266
Ewan Bauld <ewan.bauld@gmail.com>
2018-08-17 15:30:21 +0100

    Add vendors POST to API

--- routes/api/vendors.js
+++ routes/api/vendors.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 const Vendor = require('../models/vendor');
+
+const validateVendorInput = require('../../validation/vendor');
 
 // @route GET api/vendors
 // @desc Get vendors route
@@ -15,6 +18,24 @@ router.get('/', (req, res) => {
 // @route PUT api/vendors
 // @desc Get vendors route
 // @access Private
+router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+
+    console.log('Getting this far?');
+
+    const { errors, isValid } = validateVendorInput(req.body);
+    if(!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    const newVendor = new Vendor({
+        seller: req.body.seller,
+        paymentmethod: req.body.paymentmethod,
+        price: req.body.price,
+        lowerlimit: req.body.lowerlimit,
+        upperlimit: req.body.upperlimit,
+    })
+    newVendor.save().then(vendor => res.json(vendor));
+});
 
 
 
