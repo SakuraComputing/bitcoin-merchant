6274dfbd841577185423c64bb144e7f5a08d9b60
Ewan Bauld <ewan.bauld@gmail.com>
2018-08-17 13:37:56 +0100

    Style Login Form

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -54,60 +54,50 @@ class Login extends Component {
                 <div className="login">
                     <div className="row">
                         <h2 className="lead text-center">Sign in to your Bitcoin Merchant account</h2>
-
                     </div>
                     <div className="login__container">
-
-                            <form onSubmit={this.onSubmit}>
-                                <div className="row">
-                                    <div className="col span-1-of-3">
-                                        <label htmlFor="name">Email Address</label>
-                                    </div>
-                                    <div className="col span-2-of-3">
-                                        <input 
-                                            className="forms__input"
-                                            type="email" 
-                                            name="email" 
-                                            id="email" 
-                                            placeholder="Your Email" 
-                                            onChange={this.onChange}
-                                            error={errors.email}
-                                        />
-                                    </div>
+                        <form onSubmit={this.onSubmit}>
+                            <div className="row">
+                                <div className="col span-1-of-3">
+                                    <label htmlFor="name">Email Address</label>
                                 </div>
-                                <div className="row">
-                                    <div className="col span-1-of-3">
-                                        <label htmlFor="password">Password</label>
-                                    </div>
-                                    <div className="col span-2-of-3">
-                                        <input 
-                                            className="forms__input"
-                                            type="text" 
-                                            name="password" 
-                                            id="password" 
-                                            placeholder="Password" 
-                                            onChange={this.onChange}
-                                            error={errors.password}
-                                        />
-                                    </div>
+                                <div className="col span-2-of-3">
+                                    <input 
+                                        className="forms__input"
+                                        type="email" 
+                                        name="email" 
+                                        id="email" 
+                                        placeholder="Your Email" 
+                                        onChange={this.onChange}
+                                        error={errors.email}
+                                    />
                                 </div>
-                                <div className="row">
-                                    <div className="col span-1-of-3">
-                                        <label>&nbsp;</label>
-                                    </div>
-                                    <div className="col span-2-of-3">
-                                        <input className="btn btn-full" type="submit" value="Log In"/>
-                                    </div>
-                                </div>     
-                                                            
-                                <div>
-                                    <div className="col span-1-of-3">
-                                        <label>&nbsp;</label>
-                                    </div>
-                                    <div className="col span-2-of-3">
-                                        <Link className="btn btn-ghost" to="/register">Register</Link>
-                                    </div>
+                            </div>
+                            <div className="row">
+                                <div className="col span-1-of-3">
+                                    <label htmlFor="password">Password</label>
                                 </div>
+                                <div className="col span-2-of-3">
+                                    <input 
+                                        className="forms__input"
+                                        type="text" 
+                                        name="password" 
+                                        id="password" 
+                                        placeholder="Password" 
+                                        onChange={this.onChange}
+                                        error={errors.password}
+                                    />
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col span-1-of-3">
+                                    <label>&nbsp;</label>
+                                </div>
+                                <div className="col span-2-of-3 button-bar">
+                                    <input type="submit" value="Log In" className="btn btn-full input-tag"></input>
+                                    <Link className="btn btn-ghost" to="/register">Register</Link>
+                                </div>
+                            </div>     
                         </form>
                     </div>
                 </div>
