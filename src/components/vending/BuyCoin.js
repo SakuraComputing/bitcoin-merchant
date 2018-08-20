e0bfd1f848bcf95cc01586e3ca2a419b79569d37
Ewan Bauld <ewan.bauld@gmail.com>
2018-08-20 14:16:00 +0100

    Animate Buy button, add Buycoin component

---
+++ src/components/vending/BuyCoin.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class BuyCoin extends Component {
+    render() {
+        return (
+            <div>
+                Buy those Bit Coins
+            </div>
+        )
+    }
+}
