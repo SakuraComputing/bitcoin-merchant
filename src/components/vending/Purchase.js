2747b304972f3dae0896add7a3129c23834f68d5
Ewan Bauld <ewan.bauld@gmail.com>
2018-09-05 01:18:38 +0100

    Add credit card

--- src/components/vending/Purchase.js
+++ src/components/vending/Purchase.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
+import Cards from 'react-credit-cards';
 
 class Purchase extends Component {
   render() {
     return (
       <div>
-        Purchase Bitcoin
+          <h2>Purchase Bitcoin</h2>
+          <div className="row">
+            <div className="col span-1-of-2 buyer-box">
+                Amount
+            </div>
+            <div className="col span-1-of-2 buyer-box">
+                Credit Card Details
+                <Cards 
+                    number={38712682138123}
+                    name={"RP MacMurphy"}
+                    expiry={2020}
+                    cvc={328}
+                />
+            </div>
+        </div>
+        
       </div>
     )
   }
